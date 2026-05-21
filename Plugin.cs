3612964f1b992f1a42e5373f13f25b1dcb8f4806
Pyran99 <130940584+Pyran99@users.noreply.github.com>
2026-05-21 00:57:51 -0600

    added neuro sdk

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,8 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
 using BepInEx;
 using BepInEx.Logging;
+using NeuroSdk;
+using NeuroSdk.Actions;
 
 namespace NeuroFTK;
 
@@ -10,15 +11,38 @@ namespace NeuroFTK;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-    int test;
+    readonly int test = 15;
         
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-        test = 25;
+        test2(null);
+        Environment.SetEnvironmentVariable("NEURO_SDK_WS_URL", "ws://localhost:8000");
+        NeuroSdkSetup.Initialize("For the King");
+        
+    }
+
+    public void test2(ActionWindow window)
+    {
         Logger.LogInfo($"test: {test}");
+        float new_test = 30f;
+        for (int i = 0; i < 50; i++)
+        {
+            new_test++;
+        }
+        Logger.LogMessage($"test: {new_test}");
+    }
+
+    public class TestClass
+    {
+        private int test;
+        public TestClass(int test)
+        {
+            this.test = test;
+        }
     }
     
 }
+
