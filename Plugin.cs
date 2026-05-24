f9185019b7f0395b0f6aa3a2404e74c1b9be960a
Pyran99 <130940584+Pyran99@users.noreply.github.com>
2026-05-24 13:36:34 -0600

    redo

--- Plugin.cs
+++ Plugin.cs
@@ -1,49 +1,17 @@
-﻿using System;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
-using HarmonyLib;
-// using NeuroSdk;
-// using NeuroSdk.Actions;
-using UnityEngine;
 
 namespace NeuroFTK;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-[BepInProcess("FTK.exe")]
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-    readonly int test = 15;
-
+        
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-        Environment.SetEnvironmentVariable("NEURO_SDK_WS_URL", "ws://localhost:8000");
-        // NeuroSdkSetup.Initialize("For the King");
-        Test2();
     }
-
-    [HarmonyPrefix]
-    public void Test2()
-    {
-        Logger.LogInfo($"test: {test}");
-        float new_test = 30f;
-        for (int i = 0; i < 50; i++)
-        {
-            new_test++;
-        }
-        Logger.LogMessage($"test: {new_test}");
-        Test3();
-    }
-
-    [HarmonyPatch(typeof(GameObject))]
-    public static void Test3()
-    {
-        Logger.LogDebug("test3 harmony patch");
-    }
-
-    
 }
-
--- obj/Debug/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace NeuroFTK
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "NeuroFTK";
-        public const string PLUGIN_NAME = "Neuro For The King mod";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
