b34e91b55edb8f5d053fb481e0148a46b4d367c5
Pyran99 <130940584+Pyran99@users.noreply.github.com>
2026-05-31 21:08:40 -0600

    added ignores

--- obj/Debug/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace NeuroFTK
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "NeuroFTK";
-        public const string PLUGIN_NAME = "Neuro For the King";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
