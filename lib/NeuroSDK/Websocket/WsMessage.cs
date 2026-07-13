d824c8158bcabf58f4ae5b9c25d8818295b225d4
Pyran99 <130940584+Pyran99@users.noreply.github.com>
2026-07-12 20:08:35 -0600

    didnt work out

---
+++ lib/NeuroSDK/Websocket/WsMessage.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+
+using Newtonsoft.Json;
+
+namespace NeuroSdk.Websocket
+{
+    /* Missing compiler required member 'System.Type.op_Equality'
+    original -> public record WsMessage
+    */
+    public class WsMessage(string command, object? data, string game)
+    {
+        [JsonProperty("command", Order = 0)]
+        public readonly string Command = command;
+
+        [JsonProperty("game", Order = 10)]
+        private readonly string _game = game;
+
+        [JsonProperty("data", Order = 20)]
+        public readonly object? Data = data;
+    }
+}
